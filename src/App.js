fa0ee4d40b248bb1a3eb7a7dae727bab4f3d5632
Prajwal Bundhade <prajwalpro231@gmail.com>
2023-09-01 18:57:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import About from './components/About';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+
+import { BrowserRouter as Router,Routes, Route, } from 'react-router-dom';
 
 function App() {
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+        setAlert(null);
+    }, 1500);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+     
+    <Navbar title="Word Counter" aboutpage="About" />
+    <Alert alert={alert}/>
+    
+     
+      <div className="container my-6">
+      
+      <Routes>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text below"/>} />
+          <Route exact path="/about" element={<About/>} />
+          
+        </Routes>
+      </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleUpClick = ()=>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
+
+    const handleLoClick = ()=>{ 
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lowercase!", "success");
+    }
+
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
+    }
+
+    const handleOnChange = (event)=>{
+        setText(event.target.value) 
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
+    
+    const[text, setText]= useState('');
+    return (
+        <>
+        <div className='container my-3'>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
+            </div>
+            <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
+            <button type="submit" onClick={speak} className="btn btn-warning mx-2">Speak</button>
+            <button type="submit" onClick={handleClearClick} className="btn btn-warning mx-2">Clear</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        </div>
+
+        <div className="container my-3">
+            <h2>Your Text Summary here:</h2>
+            <p>{text.split(" ").length} Words and {text.length} Characters</p>
+            <p>{0.008*text.split(" ").length} Minutes read</p>
+            <h3>Preview Text:</h3>
+            <p className='container preview'>{text}</p>
+        </div>
+        </>
+        )
+    }
