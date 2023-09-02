5edbc9a1fca5c3c449c74499db212c7cd8281b72
prajwalbundhade <prajwalpro231@gmail.com>
2023-09-02 18:23:39 +0530

    minor changes in about page and homepage

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -47,19 +47,19 @@ export default function TextForm(props) {
         <div className='container my-3'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} id="myBox" onChange={handleOnChange} rows="8"></textarea>
+            <textarea className="form-control" value={text} id="myBox" placeholder='Please enter a text here' onChange={handleOnChange} rows="8"></textarea>
             </div>
-            <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
-            <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
-            <button type="submit" onClick={speak} className="btn btn-warning mx-2">Speak</button>
-            <button type="submit" onClick={handleClearClick} className="btn btn-warning mx-2">Clear</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-1 my-1' onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className='btn btn-primary mx-1 my-1' onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} type="submit" onClick={speak} className="btn btn-warning mx-1">Speak</button>
+            <button disabled={text.length===0} type="submit" onClick={handleClearClick} className="btn btn-warning mx-2">Clear</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
             <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
 
         <div className="container my-3">
             <h2>Your Text Summary here:</h2>
-            <p>{text.split(" ").length} Words and {text.length} Characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Words and {text.length} Characters</p>
             <p>{0.008*text.split(" ").length} Minutes read</p>
             <h3>Preview Text:</h3>
             <p className='container preview'>{text}</p>
